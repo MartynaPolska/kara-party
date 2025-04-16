2a0e4ba85bd8692c9dca346ceef7412a957a26c3
Wansaja Perera <wansajaperera@Wansajas-MacBook-Air.local>
2025-04-16 21:12:01 +0200

    Fix: pinwathiye sinhala lyrics

--- src/songs/pinwanthiye.js
+++ src/songs/pinwanthiye.js
@@ -1,11 +1,20 @@
 const pinwanthiye = {
     id: "pinwanthiye",
-    title: "Pinwanthiye Mage",
-    artist: "Unknown Artist",
+    title: "පින්වන්තියේ",
+    artist: "Sangeeth Wijesooriya",
     genre: "Sinhala Classic",
     audioUrl: "/api/stream?id=pinwanthiye",
-    lyrics: `Pinwanthiye mage piyamban
-  Wetha nopenena oba sanda wanaran...`
+    syncedLyrics: [
+      { time: 1.73, text: "පින්වන්තියේ මාගේ ප්‍රේම කතාවේ" },
+      { time: 4.52, text: "ඇයිදෝ මට පාවෙ මල් මද හාසේ" },
+      { time: 7.89, text: "දස සල සිත යට කොල කතා" },
+      { time: 11.42, text: "රූප ශ්‍රිය මා සිත උමුතු කරා" },
+      { time: 14.05, text: "ආදරේ පතා ඔබෙන් සිත සිතම ආවේ සොයා" },
+      { time: 17.38, text: "ජීවිතේ ගියත් ඉතින් දමලා නොයං" },
+      { time: 20.26, text: "කිසිදා දලා නොයං" },
+      { time: 22.44, text: "ආදරේ පතා ඔබෙන් සිත සිතම ආවේ සොයා" },
+      { time: 25.77, text: "ආදරෙට ආදරේ සුපෙම් දෑතා පමී ආදරේ නම්" }
+    ]
   };
   
   export default pinwanthiye;
