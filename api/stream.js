e6e6a7a7d8de8f49acece02c712a55cc730f6a75
Wansaja Perera <wansajaperera@Wansajas-MacBook-Air.local>
2025-04-16 20:23:19 +0200

    Fix: use node-fetch for audio proxy in stream.js for what was i made for

--- api/stream.js
+++ api/stream.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     "november-rain": "https://firebasestorage.googleapis.com/v0/b/kara-party-lk.firebasestorage.app/o/november-rain-cover.mp3?alt=media&token=3c08b1ed-17af-4939-84cc-e722b1b3875f",
     "pinwanthiye": "https://firebasestorage.googleapis.com/v0/b/kara-party-lk.firebasestorage.app/o/Pinwanthiye%20Mage.mp3?alt=media&token=8474d722-4b2c-47b6-b46d-e5e145a08396",
     "obe-sina": "https://firebasestorage.googleapis.com/v0/b/kara-party-lk.firebasestorage.app/o/obe-sina-laga-nuwandika-senarathne.mp3?alt=media&token=81e4675b-eaf8-416d-8677-99a2117b2038",
-    "billie-eilish": "https://firebasestorage.googleapis.com/v0/b/kara-party-lk.firebasestorage.app/o/What-Was-I-Made-For%20-Billie%20Eilish.mp3?alt=media&token=b3450d92-22e1-46f3-bc3a-b50625fc6c82"
+    "what-was-i-made-for": "https://firebasestorage.googleapis.com/v0/b/kara-party-lk.firebasestorage.app/o/What-Was-I-Made-For%20-Billie%20Eilish.mp3?alt=media&token=b3450d92-22e1-46f3-bc3a-b50625fc6c82"
   };
 
   const url = audioLinks[id];
